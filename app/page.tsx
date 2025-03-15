6630126fcc0d483af1610a4a752765304fd5e90c
sarabov <arabov.simeon1990@gmail.com>
2025-03-15 17:02:59 +0200

    fix bug and add currency symbols

--- app/page.tsx
+++ app/page.tsx
@@ -32,49 +32,7 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { Spinner } from "@/components/ui/spinner";
-
-type CurrencyCardProps = {
-  currencyData: Asset;
-  size: "lg" | "small";
-};
-
-const CurrencyCard = ({ currencyData, size }: CurrencyCardProps) => {
-  const { symbol, price, change } = currencyData;
-
-  const changeColor = change > 0 ? "text-green-500" : "text-red-500";
-
-  const sizeClasses = size === "lg" ? "w-44 p-2" : "w-36 p-1";
-  const textSize = size === "lg" ? "text-sm" : "text-xs";
-  const fontSize = size === "lg" ? "font-medium" : "font-semibold";
-
-  return (
-    <div
-      className={`flex items-center justify-between rounded-lg shadow-md bg-gray-800 ${sizeClasses}`}
-    >
-      <div className="flex flex-col items-start">
-        <span className={`text-white ${textSize} ${fontSize} font-mono`}>
-          {symbol}
-        </span>
-        <span className={`text-white ${textSize} font-mono`}>
-          ${price.toFixed(2)}
-        </span>
-      </div>
-      <div className="flex flex-col items-end">
-        <div className="flex flex-col justify-between h-full items-end">
-          <span
-            className={`w-2 h-2 rounded-full ${
-              change > 0 ? "bg-green-500" : "bg-red-500"
-            }`}
-          />
-          <div className="h-3"></div>
-          <span className={`${textSize} ${changeColor} font-mono`}>
-            {change.toFixed(2)}%
-          </span>
-        </div>
-      </div>
-    </div>
-  );
-};
+import CurrencyCard from "@/components/currency";
 
 function daysAgoToDate(days: number): string {
   const today = new Date();
